5e1ab0b17e113fca54d5682346d75f8f599e2b4b
unknown <ngounouarmel45@yahoo.fr>
2023-11-08 00:15:03 -0500

    fini avec le premier niveau

--- Devoir2/RechercheLocale/solver.py
+++ Devoir2/RechercheLocale/solver.py
@@ -3,32 +3,52 @@ from uflp import UFLP
 from typing import List, Tuple
 import random
 import itertools
+import numpy as np
 
 
 def solve(problem: UFLP) -> Tuple[List[int], List[int]]:
-
+    
+    objecitf=[]
     temp = 100
     cooling_rate = 0.99
-    
-    #Solution initiale
-    main_stations_opened = [random.choice([0, 1]) for _ in range(problem.n_main_station)]
-    if(sum(main_stations_opened) == 0):
-        main_stations_opened[random.randint(0, problem.n_main_station - 1)] = 1
-    index = [i for i, x in enumerate(main_stations_opened) if x == 1]
-    best_satellites = [random.choice(index) for _ in range(problem.n_satellite_station)]
-    best_stations = main_stations_opened
-    best_cost = problem.calcultate_cost(best_stations, best_satellites)
 
-
+    #solution initiale
+    best_stations,best_satellites,best_cost=generat_init_solution(problem)
+    objecitf.append((best_stations,best_satellites, best_cost))
     for i in range(5000):
-        new_sation, new_sattelite = local_search(problem, best_stations, temp, cooling_rate,best_cost)
-        new_cost = problem.calcultate_cost(best_stations, best_satellites)
+        new_sation, new_sattelite,new_cost = local_search(problem, best_stations, temp, cooling_rate,best_cost)
         if new_cost < best_cost:
-            best_cost = new_cost
-            best_stations = new_sation
-            best_satellites = new_sattelite
-        i+=1
-    return best_stations, best_satellites
+            if problem.solution_checker(new_sation,new_sattelite):
+                best_cost = new_cost
+                best_stations = new_sation
+                best_satellites = new_sattelite
+                objecitf.append((best_stations,best_satellites, best_cost))
+
+        else:
+            b_stations,satellite,best_cost=generat_init_solution(problem)
+            if problem.solution_checker(b_stations,satellite):
+                if (b_stations,satellite, best_cost) not in objecitf:
+                    objecitf.append((b_stations,satellite, best_cost))
+    #objectif
+    best_stations,best_satellites,_=min(objecitf, key=lambda x: x[2])
+    return list(best_stations), best_satellites
+
+def generat_init_solution(problem:UFLP):
+
+    main_stations_state = [random.choice([0, 1]) for _ in range(problem.n_main_station)]
+    #contrainte sur le fait qu'au moins une station principale doit être ouvert
+    if(sum(main_stations_state) == 0):
+        main_stations_state[random.randint(0, problem.n_main_station - 1)] = 1
+    #indexer les stations principales qui sont ouvertes
+    index_main_stations_opened = [i for i, x in enumerate(main_stations_state) if x == 1]
+
+    best_satellites = [random.choice(index_main_stations_opened) for _ in range(problem.n_satellite_station)]
+
+    best_stations = main_stations_state
+    cost = problem.calcultate_cost(best_stations, best_satellites)
+
+    return best_stations,best_satellites,cost
+
 
 def local_search(problem: UFLP, stations, temp, cooling_rate,best_cost):
     
@@ -39,37 +59,28 @@ def local_search(problem: UFLP, stations, temp, cooling_rate,best_cost):
         for index,satelite in enumerate(permutation):
             new_cost = problem.calcultate_cost(permutation, neighbor_sattelite[index])
             delta_cost = new_cost - best_cost
-          
-            if delta_cost < 0  or random.random()< math.exp(-delta_cost / temp) :
+            if delta_cost < 0:
                 best_stations = permutation
                 best_satellites = neighbor_sattelite[index]
                 best_cost = new_cost
-            # if new_cost < best_cost:
-            #     best_cost = new_cost
-            #     best_stations = permutation
-            #     best_satellites = neighbor_sattelite[index]
-        temp *= cooling_rate
-    return best_stations, best_satellites
+            elif random.random()< math.exp(-delta_cost / temp):
+                best_stations = permutation
+                best_satellites = neighbor_sattelite[index]
+                best_cost = new_cost
+                temp *= cooling_rate
+    return best_stations, best_satellites,best_cost
 
 def neighbor(problem: UFLP,stations):
     unique_permutations = set()
     satellite_combinations = []
-    # if(len(stations) <=10):
-    #     for perm in itertools.permutations(stations, len(stations)):
-    #         if perm not in unique_permutations:
-    #             unique_permutations.add(perm)
-    #     for perm in unique_permutations:
-    #         index = [i for i, x in enumerate(perm) if x == 1]
-    #         satellite_combinations.append([random.choice(index) for _ in range(problem.n_satellite_station)])
-    #     return list(unique_permutations),satellite_combinations
-    # else:
-    for _ in range(1000):  
-        station_permutation = next(itertools.permutations(stations, len(stations)))
+  
+    for station_permutation in itertools.permutations(stations, problem.n_satellite_station):
         if station_permutation not in unique_permutations:
             unique_permutations.add(tuple(station_permutation))
         for perm in unique_permutations:
             index = [i for i, x in enumerate(perm) if x == 1]
-            satellite_combinations.append([random.choice(index) for _ in range(problem.n_satellite_station)])
+            #index_open_cost=index[np.argmin([problem.get_opening_cost(index[j])for j in range(len(index))])]       
+            satellite_combinations.append([random.choices(index) for _ in range(problem.n_satellite_station)])
 
     return list(unique_permutations), satellite_combinations
 
